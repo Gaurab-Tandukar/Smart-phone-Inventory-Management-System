1d3bfea2a553a7ec9f7d98bc5b205a79676b8046
Gaurab Tandukar <gaurabtandukar20@gmail.com>
2026-02-21 21:35:51 +0545

    Added Update Feeature

--- model/StoreController.java
+++ model/StoreController.java
@@ -48,12 +48,89 @@ public class StoreController {
                 found = true;
                 System.out.println("Product deleted successfully.");
                 break;
+            }
         }
-    }
 
         if (!found) {
             System.out.println("Model Code not found.");
         }
+    }
+
+    //Update product code
+    public void updateProduct() {
+        System.out.println("Enter Model Code: \n");
+        int mCode = sc.nextInt();
+        sc.nextLine();
+
+        if (phoneList.isEmpty()) {
+            System.out.println("Model Code not found\n");
+        }
+
+        boolean found = false;
+
+        for (int i = 1; i < phoneList.size(); i++) {
+            if (phoneList.get(i).getModelCode() == mCode) {
+
+                //model code found now what to update
+                boolean exit = false;
+                int choice = 0;
+                while (!exit) {
+                    System.out.println("Enter data to update?");
+                    System.out.println("------------------------------");
+                    System.out.println("1 -> Model Name");
+                    System.out.println("2 -> Quantity");
+                    System.out.println("3 -> Price");
+                    System.out.println("4 -> Operating System");
+                    System.out.println("5 -> Connectivity");
+                    System.out.println("6 -> Exit");
+                    System.out.println("------------------------------");
+                    choice = sc.nextInt();
+                    sc.nextLine();
+
+                    switch (choice) {
+                        case (1):
+                            System.out.println("Enter Model Name:\n");
+                            String mName = sc.nextLine();
+                            phoneList.get(i).setModelName(mName);
+                        break;
+                        case (2):
+                            System.out.println("Enter Model Quantity:\n");
+                            int mQyt = sc.nextInt();
+                            phoneList.get(i).setModelQty(mQyt);
+                        break;
+                        case (3):
+                            System.out.println("Enter Model Price:\n");
+                            float mPrice = sc.nextFloat();
+                            phoneList.get(i).setModelPrice(mPrice);
+                        break;
+                        case (4):
+                            System.out.println("Enter Model Operating System:\n");
+                            String mOS = sc.nextLine();
+                            phoneList.get(i).setOS(mOS);
+                        break;
+                        case (5):
+                            System.out.println("Enter Model Connectivity:\n");
+                            String mConn = sc.nextLine();
+                            phoneList.get(i).setConnectivity(mConn);
+                        break;
+                        case (6):
+                            exit = true;
+                        break;
+                        default:
+                            System.out.println("Enter Valid Choice!");
+                        break;
+                    }
+                }
+
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Model Code not found!\n");
+        }
+
     }
 
 }
